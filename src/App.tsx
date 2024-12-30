9e93cb2d0f2d85f699b6f216e427b6a62e880907
urke90 <urosbijelic90@gmail.com>
2024-12-30 12:58:37 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// ----------------------------------------------------------------
+
+const App = () => {
+  return (
+    <div>
+      <h1>Health Records App</h1>
+    </div>
+  );
+};
+
+export default App;
