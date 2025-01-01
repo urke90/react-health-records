cfe3b124468af5c2c8ca21020f45f741cced7f3c
urke90 <urosbijelic90@gmail.com>
2025-01-01 13:00:36 +0100

    create NotFound.tsx component for rendering non existent route

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,23 @@
-type Props = {};
+import NotFoundIcon from '@/components/icons/NotFound';
+import { Link } from 'react-router';
 
-const NotFound = (props: Props) => {
-  return <div>NotFound</div>;
+// ----------------------------------------------------------------
+
+const NotFound: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="min-w-[320px]  p-3 text-center flex flex-col  items-center">
+        <h3 className="text-xl sm:text-3xl">404 Page Not Found</h3>
+        <NotFoundIcon className="-mt-6" />
+        <div className="flex gap-2 items-center">
+          Go back
+          <Link className="bg-cyan-500 px-3 py-2 text-white rounded-md" to="/">
+            Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
