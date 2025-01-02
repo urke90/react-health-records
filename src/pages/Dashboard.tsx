58d6122a7105b1ffbb8e461a85cae7e5970d1a1a
urke90 <urosbijelic90@gmail.com>
2025-01-02 14:40:40 +0100

    finish Button.tsx component with primary and outlined variants

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,9 @@
 type Props = {};
 
-import Button from '@/components/ui/Button';
-
 const Dashboard = (props: Props) => {
   return (
     <div className="p-5">
       <p className="mb-2">Dashboard Page</p>
-      <Button onClick={() => alert('radi')}>Button</Button>
     </div>
   );
 };
