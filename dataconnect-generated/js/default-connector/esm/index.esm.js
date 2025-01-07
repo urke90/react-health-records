b7835021f261cd17c9e7982e25baf484182a4b8d
urke90 <urosbijelic90@gmail.com>
2025-01-07 23:24:45 +0100

    setup auth emulator, add function for creating user with email and password

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'health-records-toma-test',
+  location: 'us-central1'
+};
+
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,20 +1,36 @@
 import Button from '@/components/ui/Button';
 import Input from '@/components/ui/Input';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { Link } from 'react-router';
 
 // ----------------------------------------------------------------
 
 const Register: React.FC = () => {
-  const handleSubmit = () => {};
+  const auth = getAuth();
+
+  const handleSubmit = async () => {
+    try {
+      const response = await createUserWithEmailAndPassword(auth, '', '');
+    } catch (error) {
+      console.log('Error creating user:', error);
+    }
+  };
 
   return (
-    <div className="border border-blue-500 w-[min(400px,100%)] p-2 sm:p-4">
+    <div className="flex flex-col gap-3 border border-blue-500 w-[min(400px,100%)] p-2 sm:p-4">
       <h2 className="h2-bold text-center">Sign Up</h2>
-      <form onSubmit={handleSubmit}>
-        <Input type="text" placeholder="Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+        <Input type="text" placeholder="Name" label="Your Name" />
+        <Input type="email" placeholder="Email" label="Your Email" />
+        <Input type="password" placeholder="Password" label="Your Password" />
         <Button className="w-full">Sign Up</Button>
       </form>
+      <div className="flex flex-col gap-2 text-center border">
+        <p className="p2-medium text-center w-full">Or if you already have an account instead</p>
+        <Link to="/login" className="text-blue-500 underline">
+          Sign In
+        </Link>
+      </div>
     </div>
   );
 };
