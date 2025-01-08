b8a5d0b168ea5f7e3b766c4b97c84bc238629230
urke90 <urosbijelic90@gmail.com>
2025-01-08 23:07:05 +0100

    code cleaning for AuthLayout.tsx

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router';
 
 // ----------------------------------------------------------------
 
-const AuthLayout = () => {
+const AuthLayout: React.FC = () => {
   return (
     <div className="min-h-screen flex-center bg-cyan-50">
       <Outlet />
