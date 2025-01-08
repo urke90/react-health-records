de50316cb95722e0190c63683e2d677b353be9ae
urke90 <urosbijelic90@gmail.com>
2025-01-08 12:57:45 +0100

    split name as field to first and last name, update register form interface and zod validation schema add input for last name

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,22 +1,18 @@
 import Button from '@/components/ui/Button';
 import Input from '@/components/ui/Input';
 import { auth } from '@/db';
-import { registerFormSchema } from '@/lib/validation';
+import { type IRegisterForm, registerFormSchema } from '@/lib/validation';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { FirebaseError } from 'firebase/app';
+import { AuthErrorCodes, createUserWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { Link } from 'react-router';
 
 // ----------------------------------------------------------------
 
-interface IRegisterForm {
-  email: string;
-  password: string;
-  name: string;
-}
-
 const Register: React.FC = () => {
-  console.log('auth', auth);
+  const [error, setError] = useState<string | null>(null);
 
   const {
     register,
@@ -26,39 +22,52 @@ const Register: React.FC = () => {
   } = useForm({
     resolver: zodResolver(registerFormSchema),
     defaultValues: {
+      firstName: '',
+      lastName: '',
       email: '',
       password: '',
-      name: '',
     },
   });
 
   const onSubmit: SubmitHandler<IRegisterForm> = async (data) => {
     try {
-      console.log('data', data);
-      const { email, name, password } = data;
-
-      console.log('name', name);
-      console.log('email', email);
-      console.log('password', password);
+      // console.log('data', data);
+      const { email, firstName, lastName, password } = data;
 
       const response = await createUserWithEmailAndPassword(auth, email, password);
       console.log('response', response);
     } catch (error) {
-      // console.log('Error message', error.message);
-      console.log('Error creating user:', error);
+      console.log('Error message', error);
+      // ? Da li je potrebno proveravati sve errore ili samo ovako neke kao email? Da li onda praviti neku klasu gde cu imati sve error.code i njihove specificne greske ali formatirane u  userfriendly formatu??? (ovo je vise primer, da li moram da proveravam pojedinacno ili mogu da setujem u error ceo error.message i samo da renderujem?)
+      if (error instanceof FirebaseError) {
+        if (error.code === AuthErrorCodes.EMAIL_EXISTS) {
+          setError('Email is already in use!');
+        } else {
+          setError(error.message);
+        }
+      }
+      // console.log('Error creating user:', error);
     }
   };
 
   return (
     <div className="flex flex-col gap-3 shadow-xl bg-white w-[min(400px,100%)] p-2 sm:p-5">
       <h2 className="h2-bold text-center">Sign Up</h2>
+      {error && <p className="p1-medium text-red-500 text-center">{error}</p>}
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
         <Input
-          {...register('name')}
+          {...register('firstName')}
           type="text"
-          placeholder="Name"
-          label="Your Name"
-          errorMessage={errors.name?.message}
+          placeholder="First Name"
+          label="First Name"
+          errorMessage={errors.firstName?.message}
+        />
+        <Input
+          {...register('lastName')}
+          type="text"
+          placeholder="Last Name"
+          label="Last Name"
+          errorMessage={errors.lastName?.message}
         />
         <Input
           {...register('email')}
