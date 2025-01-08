421e6cf9e815586b172cd9d5ee7a3a7396bfa294
urke90 <urosbijelic90@gmail.com>
2025-01-08 23:05:12 +0100

    change bg color for AuthLayout component

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const AuthLayout = (props: Props) => {
   return (
-    <div className="min-h-screen flex-center bg-gray-50">
+    <div className="min-h-screen flex-center bg-cyan-50">
       <Outlet />
     </div>
   );
