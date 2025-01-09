c69dc0deca8b975f181589a16646b9f545ec5b14
urke90 <urosbijelic90@gmail.com>
2025-01-09 15:01:43 +0100

    remove .env file and firebase emulator generated files from github and prevent tracking the same

--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'health-records-toma-test',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
