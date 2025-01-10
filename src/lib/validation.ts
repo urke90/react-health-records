03432d9dc21b200fe670b32d3ba99f6fd6defb22
urke90 <urosbijelic90@gmail.com>
2025-01-10 13:04:15 +0100

    add fetch logic to fetch current user document from firestore

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 // ----------------------------------------------------------------
 
-/********************* AUTH  ********************************/
+/******************************** AUTH  ********************************/
 
 export const registerFormSchema = z.object({
   email: z.string().trim().email('Please enter valid email address'),
@@ -19,4 +19,34 @@ export const loginFormSchema = z.object({
 
 export type ILoginForm = z.infer<typeof loginFormSchema>;
 
-/********************* AUTH  ********************************/
+/******************************** AUTH  ********************************/
+
+/******************************** USER  ********************************/
+
+export const userProfileSchema = z.object({
+  // id: z.string().trim();
+  userName: z.string().trim().min(3, 'Username is required and must be at least 3 characters long'),
+  firstName: z
+    .string()
+    .trim()
+    .min(3, 'First name is required and must be at least 3 characters long'),
+  lastName: z
+    .string()
+    .trim()
+    .min(3, 'Last name is required and must be at least 3 characters long'),
+  birthDate: z.date({ required_error: 'Date of birth is required' }).optional(),
+  profileImg: z.string().trim().url().optional(),
+  email: z.string().trim().email('Please provide valid email address'),
+  address: z.object({
+    state: z.string().trim().min(3, 'State is required'),
+    city: z.string().trim().min(3, 'City is required'),
+    street: z.string().trim().min(3, 'Street is required'),
+    phone: z.string().trim().min(3, 'Phone is required'),
+  }),
+  allergies: z.string().trim().optional(),
+  specialNotes: z.string().trim().optional(),
+});
+
+export type IUserProfileSchema = z.infer<typeof userProfileSchema>;
+
+/******************************** USER  ********************************/
--- src/pages/ProfileEdit.tsx
+++ src/pages/ProfileEdit.tsx
@@ -1,15 +1,53 @@
 import Input from '@/components/ui/Input';
 import Textarea from '@/components/ui/Textarea';
+import { auth, db } from '@/db';
+import { type IUserProfileSchema } from '@/lib/validation';
+import { doc, type DocumentData, getDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 // ----------------------------------------------------------------
 
 type Props = {};
 
 const ProfileEdit = (props: Props) => {
+  const [data, setData] = useState<DocumentData>();
+
+  const userId = auth.currentUser?.uid;
+
+  const { handleSubmit } = useForm<IUserProfileSchema>({
+    defaultValues: {},
+  });
+
+  const onSubmit: SubmitHandler<IUserProfileSchema> = (data) => {
+    console.log('data u onSubmit', data);
+  };
+
+  useEffect(() => {
+    if (!userId) return;
+    const fetchUserDoc = async () => {
+      const userDocRef = doc(db, 'users', userId);
+      const userDocSnap = await getDoc(userDocRef);
+
+      if (userDocSnap.exists()) {
+        console.log('userDoc response', userDocSnap.data());
+
+        const userData = userDocSnap.data();
+
+        setData(userData);
+      }
+    };
+    fetchUserDoc();
+  }, [userId]);
+
+  useEffect(() => {
+    console.log('data', data);
+  }, [data]);
+
   return (
     <section className="flex flex-col gap-2 sm:gap-4 flex-1 m-auto max-sm:w-[min(460px,100%)]">
-      <h2 className="h2-bold">Uros Bijelic Health Card</h2>
-      <form className="md:flex gap-6 lg:gap-10">
+      <h2 className="h2-bold">Your Health Card</h2>
+      <form className="md:flex gap-6 lg:gap-10" onSubmit={handleSubmit(onSubmit)}>
         <div className="w-[min(400px,100%)]">
           <p className="p1-bold underline mb-2">Personal Info</p>
           <div className="flex flex-col gap-2">
