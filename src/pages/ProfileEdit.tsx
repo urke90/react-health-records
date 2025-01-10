d847fd9b1d9ed0b450d5e495a36bc325545f367c
urke90 <urosbijelic90@gmail.com>
2025-01-10 16:04:30 +0100

    add register methods from react hook form and error messages to each input

--- src/pages/ProfileEdit.tsx
+++ src/pages/ProfileEdit.tsx
@@ -1,10 +1,15 @@
 import Input from '@/components/ui/Input';
 import Textarea from '@/components/ui/Textarea';
 import { auth, db } from '@/db';
-import { type IUserProfileSchema, type IUserProfileSchemaDTO } from '@/lib/validation';
+import {
+  userProfileSchema,
+  type IUserProfileSchema,
+  type IUserProfileSchemaDTO,
+} from '@/lib/validation';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { doc, getDoc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
-import { type SubmitHandler, useForm } from 'react-hook-form';
+import { useForm, type SubmitHandler } from 'react-hook-form';
 
 // ----------------------------------------------------------------
 
@@ -15,7 +20,13 @@ const ProfileEdit = (props: Props) => {
 
   const userId = auth.currentUser?.uid;
 
-  const { handleSubmit } = useForm<IUserProfileSchema>({
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    watch,
+  } = useForm<IUserProfileSchema>({
+    resolver: zodResolver(userProfileSchema),
     defaultValues: {
       firstName: data?.firstName || '',
       lastName: data?.lastName || '',
@@ -33,6 +44,8 @@ const ProfileEdit = (props: Props) => {
       },
     },
   });
+
+  console.log('watch', watch());
 
   const onSubmit: SubmitHandler<IUserProfileSchema> = (data) => {
     console.log('data u onSubmit', data);
@@ -72,21 +85,71 @@ const ProfileEdit = (props: Props) => {
         <div className="w-[min(400px,100%)]">
           <p className="p1-bold underline mb-2">Personal Info</p>
           <div className="flex flex-col gap-2">
-            <Input label="First Name" placeholder="First name" />
-            <Input label="Last Name" placeholder="Last name" />
-            <Input label="Userame" placeholder="Username" />
-            <Input label="Email" placeholder="Email" />
-            <Textarea placeholder="Allergies" label="Allergies" />
-            <Textarea placeholder="Special Notes" label="Special Notes" />
+            <Input
+              label="First Name"
+              placeholder="First name"
+              {...register('firstName')}
+              errorMessage={errors.firstName?.message}
+            />
+            <Input
+              label="Last Name"
+              placeholder="Last name"
+              {...register('lastName')}
+              errorMessage={errors.lastName?.message}
+            />
+            <Input
+              label="Userame"
+              placeholder="Username"
+              {...register('userName')}
+              errorMessage={errors.userName?.message}
+            />
+            <Input
+              label="Email"
+              placeholder="Email"
+              {...register('email')}
+              errorMessage={errors.email?.message}
+            />
+            <Textarea
+              placeholder="Allergies"
+              label="Allergies"
+              {...register('allergies')}
+              errorMessage={errors.allergies?.message}
+            />
+            <Textarea
+              placeholder="Special Notes"
+              label="Special Notes"
+              {...register('specialNotes')}
+              errorMessage={errors.specialNotes?.message}
+            />
           </div>
         </div>
         <div className="w-[min(400px,100%)]">
           <p className="p1-bold underline mb-2">Address</p>
           <div className="flex flex-col gap-2">
-            <Input label="State" placeholder="State" />
-            <Input label="City" placeholder="City" />
-            <Input label="Street" placeholder="Street" />
-            <Input label="Phone" placeholder="Phone" />
+            <Input
+              label="State"
+              placeholder="State"
+              {...register('address.state')}
+              errorMessage={errors.address?.state?.message}
+            />
+            <Input
+              label="City"
+              placeholder="City"
+              {...register('address.city')}
+              errorMessage={errors.address?.city?.message}
+            />
+            <Input
+              label="Street"
+              placeholder="Street"
+              {...register('address.street')}
+              errorMessage={errors.address?.street?.message}
+            />
+            <Input
+              label="Phone"
+              placeholder="Phone"
+              {...register('address.phone')}
+              errorMessage={errors.address?.phone?.message}
+            />
           </div>
         </div>
       </form>
