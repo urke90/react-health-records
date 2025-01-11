9147ad86a3f724575e168d0eec86de128c9af229
urke90 <urosbijelic90@gmail.com>
2025-01-11 20:14:26 +0100

    add tanstack query, initialize in App.tsx, add Devtools for tanstack query in development mode, add eslint plugin for tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer } from 'react-toastify';
 import Router from './router/Router';
 
 // ----------------------------------------------------------------
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ToastContainer />
       <Router />
-    </>
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
   );
 };
 
