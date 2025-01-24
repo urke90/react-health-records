023c692c7feb5e05080aa296760c3b90228aa2a5
urke90 <urosbijelic90@gmail.com>
2025-01-24 14:44:10 +0100

    create Checkbox component from Radix UI and RHFCheckbox that usses react hook form Controller component so it could be connected to a react hook form

---
+++ src/components/ui/Checkbox.tsx
@@ -0,0 +1,41 @@
+import * as RadixCheckbox from '@radix-ui/react-checkbox';
+
+// ----------------------------------------------------------------
+
+export interface ICheckboxProps extends RadixCheckbox.CheckboxProps {
+  label: string;
+}
+
+const Checkbox: React.FC<ICheckboxProps> = ({ label, id = '', ...rest }) => {
+  return (
+    <div className="flex items-center">
+      <RadixCheckbox.Root
+        className="flex size-5 appearance-none items-center justify-center rounded border-2 border-cyan-400 data-[state=checked]:bg-cyan-400"
+        id={id}
+        {...rest}
+      >
+        <RadixCheckbox.Indicator className="text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-check"
+          >
+            <path d="M20 6 9 17l-5-5" />
+          </svg>
+        </RadixCheckbox.Indicator>
+      </RadixCheckbox.Root>
+      <label className="pl-[15px] text-[15px] leading-none text-white" htmlFor={id}>
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default Checkbox;
