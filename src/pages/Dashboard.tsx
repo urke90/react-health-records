6f20372b60554b760b44c6ada404dfc65c663a15
urke90 <urosbijelic90@gmail.com>
2025-02-02 17:21:51 +0100

    add user name or full name and links to the create records page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,47 @@
-// import { useFetchUser } from '@/lib/hooks/queries/use-fetch-user';
+import LoadingSpinner from '@/components/ui/LoadingSpinner';
+import { useFetchUser } from '@/lib/hooks/queries/use-fetch-user';
+import { Link } from 'react-router';
 
 // ----------------------------------------------------------------
 
 const Dashboard: React.FC = () => {
-  // const { data, isPending } = useFetchUser();
+  const { data, isPending } = useFetchUser();
 
   // console.log('data', data);
   // console.log('isPending', isPending);
 
+  if (isPending) {
+    return <LoadingSpinner asOverlay />;
+  }
+
+  let name;
+
+  if (data?.firstName && data.lastName) {
+    name = `${data.firstName} ${data.lastName}`;
+  } else {
+    name = data?.userName;
+  }
+
   return (
-    <div>
-      <h2 className="h2-bold">Dashboard Page</h2>
-    </div>
+    <section className="p-3">
+      <div className="flex-between flex-wrap gap-2">
+        <h2 className="h2-bold">Hey there {name}.</h2>
+        <div className="flex flex-col gap-0.5">
+          <p className="p2-regular">Create new:</p>
+          <div className="flex gap-2 flex-wrap">
+            <Link className="bg-violet-500 px-3 py-1 rounded-md transition text-white" to="/">
+              Examination
+            </Link>
+            <Link className="bg-violet-500 px-3 py-1 rounded-md transition text-white" to="/">
+              Vaccination
+            </Link>
+            <Link className="bg-violet-500 px-3 py-1 rounded-md  transition text-white" to="/">
+              Medicine
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
